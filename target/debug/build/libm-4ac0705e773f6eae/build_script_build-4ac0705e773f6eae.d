bb19f2dc09073f74f3fcdc9369cc2c58d240caac
Gongzw <gongzw@foxmail.com>
2026-08-07 07:27:32 +0800

    chore: stop tracking build artifacts under target/

--- target/debug/build/libm-4ac0705e773f6eae/build_script_build-4ac0705e773f6eae.d
+++
@@ -1,6 +0,0 @@
-d:\project\mcu\oop\flyctrl\target\debug\build\libm-4ac0705e773f6eae\build_script_build-4ac0705e773f6eae.d: C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libm-0.2.16\build.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libm-0.2.16\configure.rs
-
-d:\project\mcu\oop\flyctrl\target\debug\build\libm-4ac0705e773f6eae\build_script_build-4ac0705e773f6eae.exe: C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libm-0.2.16\build.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libm-0.2.16\configure.rs
-
-C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libm-0.2.16\build.rs:
-C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libm-0.2.16\configure.rs:
--- target/debug/build/num-traits-574ba6f9f02c741c/build_script_build-574ba6f9f02c741c.d
+++
@@ -1,5 +0,0 @@
-d:\project\mcu\oop\flyctrl\target\debug\build\num-traits-574ba6f9f02c741c\build_script_build-574ba6f9f02c741c.d: C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\num-traits-0.2.19\build.rs
-
-d:\project\mcu\oop\flyctrl\target\debug\build\num-traits-574ba6f9f02c741c\build_script_build-574ba6f9f02c741c.exe: C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\num-traits-0.2.19\build.rs
-
-C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\num-traits-0.2.19\build.rs:
